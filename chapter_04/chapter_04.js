44cc4c765c299c27252cad0977aeaa2f8a44b3aa
zxczoxc125 <zxczoxc125@gmail.com>
2019-09-30 22:32:33 +0900

    [chapter_04] zxczoxc125 - Queue, priority Queue

---
+++ chapter_04/chapter_04.js
@@ -0,0 +1,90 @@
+/**
+ * Queue
+ * @constructor
+ */
+const Queue = function () {
+    let items = [];
+
+    this.dequeue = function () {
+        return items.shift();
+    };
+    this.enqueue = function (item) {
+        items.push(item);
+    };
+    this.front = function () {
+        return items[0];
+    };
+    this.isEmpty = function () {
+        return items.length === 0;
+    };
+    this.size = function () {
+        return items.length;
+    };
+    this.print = () => {
+        console.log(items.toString());
+    };
+};
+
+/**
+ * priorityQueue
+ * : 넣을때 우선순위를 비교해 정렬한다.
+ */
+const PriorityQueue = function () {
+    let items = [];
+
+    const Item = function (value, priority) {
+        this.value = value;
+        this.priority = priority;
+    };
+
+    this.enqueue = function (value, priority) {
+        const item = new Item(value, priority);
+
+        if (this.isEmpty()) {
+            items.push(item);
+        } else {
+            let isAdded = false;
+
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].priority < item.priority) {
+                    items.push(item);
+                    isAdded = true;
+                    return;
+                } else {
+                    /** 우선순위가 낮거나 같으면 pass */
+                }
+            }
+
+            if (!isAdded) { /** items의 제일 후순위 객체와 우선순위가 같을 경우 */
+                items.push(item);
+            }
+        }
+    };
+
+    // 이하 Queue와 동일
+    this.dequeue = function () {
+        return items.shift();
+    };
+    this.enqueue = function (item) {
+        items.push(item);
+    };
+    this.front = function () {
+        return items[0];
+    };
+    this.isEmpty = function () {
+        return items.length === 0;
+    };
+    this.size = function () {
+        return items.length;
+    };
+    this.print = () => {
+        console.log(items.toString());
+    };
+};
+
+let priorityQueue = new PriorityQueue();
+
+priorityQueue.enqueue('Jhon', 2);
+priorityQueue.enqueue('Jack', 1);
+priorityQueue.enqueue('Camila', 1);
+priorityQueue.print();
---
+++ chapter_04/chapter_04.md
@@ -0,0 +1,3 @@
+ # ✅ chapter_04
+## Queue, 우선순위 Queue, 환형 Queue
+> [chapter_04.js](https://github.com/zxczoxc125/Learning-JavaScript-Data-Structures-and-Algorithms/blob/zxczoxc125/chapter_04/chapter_04.js)
