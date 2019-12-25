8ea0d15a525b47b81eaadcab4322a576068ee6a9
zxczoxc125@naver.com <dlwo9856%^>
2019-12-25 14:31:39 +0900

    [chapter_10] - selection sort

--- chapter_10/chapter_10.js
+++ chapter_10/chapter_10.js
@@ -1,16 +1,17 @@
+const array = [1, 100, 4, 10, 56];
+
 /**
  * 1. 버블 정렬
- * @param array
  */
 const bubbleSort = () => {
     for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length - 1; j++) {
-            sort(i, j);
+        for (let j = 0; j < array.length - 1 - i; j++) {
+            sort_bubble(j, j + 1);
         }
     }
 }
 
-const sort = (i, j) => {
+const sort_bubble = (i, j) => {
     if (array[i] < array[j]) {
         const temp = array[i];
         array[i] = array[j];
@@ -18,6 +19,28 @@ const sort = (i, j) => {
     }
 }
 
-const array = [1, 100, 4, 10, 56];
-bubbleSort();
-console.log(array)
+// console.log(array);
+// bubbleSort();
+// console.log(array);
+
+/**
+ * 2. 선택 정렬
+ */
+const selectionSort = () => {
+    for (let i = 0; i < array.length; i++) {
+        let minIdx = i;
+        for (let j = i; j < array.length; j++) {
+            if (array[minIdx] > array[j]) {
+                minIdx = j;
+            }
+        }
+
+        const temp = array[minIdx];
+        array[minIdx] = array[i];
+        array[i] = temp;
+    }
+}
+
+// console.log(array);
+// selectionSort();
+// console.log(array);
